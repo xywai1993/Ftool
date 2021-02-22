efaa7dd22ffe67e80c376a4801266278dc8f6b39
yiper <yiper.fan@gmail.com>
2021-02-22 16:02:57 +0800

    胜利在望

--- src/Ftool.ts
+++ src/Ftool.ts
@@ -3,7 +3,7 @@
  */
 
 const ua = navigator.userAgent.toUpperCase();
-const Agents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPad', 'iPod'];
+const Agents = ['Android', 'iPhone', 'Windows Phone', 'iPad', 'iPod'];
 
 export const IS_ANDROID = ua.indexOf('ANDROID') !== -1;
 export const IS_IOS = ua.indexOf('IPHONE OS') !== -1;
